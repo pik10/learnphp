5a6bd30f68ce1eba656f4885daefecf75b285ad6
Peter G <petergmail@hotmail.com>
2021-12-01 18:47:40 +0000

    added the switch statement

---
+++ switch.php
@@ -0,0 +1,24 @@
+<?php
+$day = "Tue";
+
+switch ($day){
+    case "Mon":
+        echo "Monday";
+        break;
+    case "Tue":
+        echo "Tuesday";
+        break;
+    case "Wed":
+        echo "Wednesday";
+        break;
+    case "Thu":
+        echo "Thursday";
+        break;
+    case "Fri":
+        echo "Friday";
+        break;
+    default:
+        echo "No information available";
+        break;
+}
+?>
